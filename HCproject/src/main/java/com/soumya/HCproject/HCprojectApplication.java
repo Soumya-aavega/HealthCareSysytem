afd54d42bae707e4c9e30768ce679acad104982d
Soumya-aavega <108870970+Soumya-aavega@users.noreply.github.com>
2025-05-15 12:57:12 +0530

    Add files via upload
    First Commit

---
+++ HCproject/src/main/java/com/soumya/HCproject/HCprojectApplication.java
@@ -0,0 +1,13 @@
+package com.soumya.HCproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HCprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HCprojectApplication.class, args);
+	}
+
+}
---
+++ HCproject/src/test/java/com/soumya/HCproject/HCprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soumya.HCproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HCprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
